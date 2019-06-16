83c561ecd4be09f953935820e185c50d937d410f
Troy Tschirhart <troytschirhart@Troys-MacBook-Pro.local>
2019-06-16 09:49:22 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,31 @@
-console.log("Up and running!");
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[3];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2)
+{
+	if (cardsInPlay[0] === cardsInPlay[1])
+	{
+		alert("You found a match!");
+	}
+	else
+	{
+		alert("Sorry, try again.");
+	}
+}	
+
+
+
+
+
