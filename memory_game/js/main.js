1d73484415e496f134d78623688e5e39d49a7511
Troy Tschirhart <troytschirhart@Troys-MacBook-Pro.local>
2019-06-20 10:49:02 -0600

    deleted unneeded console.log statements

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -54,12 +54,7 @@ function flipCard()
 
 var cardId = this.getAttribute('data-id');
 
-console.log("User flipped " + cards[cardId].rank);
-
 cardsInPlay.push(cards[cardId].rank);
-
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
 
 this.setAttribute('src', cards[cardId].cardImage);
 
